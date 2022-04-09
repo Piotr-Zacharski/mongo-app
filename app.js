8b36d9b66998cdcffec04a9941cb43e70f2c4b4f
Piotr-Zacharski <zacharski.p.h@gmail.com>
2022-04-09 12:41:13 +0200

    Minor changes

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
-const express = require('express')
-const { connectToDb, getDb } = require('./db')
+const express = require("express");
+const { connectToDb, getDb } = require("./db");
 
+const app = express();
 
-const app = express()
-
-let db
+let db;
 
 connectToDb((err) => {
   if (!err) {
     app.listen(3000, () => {
-    console.log('app listening on port 3000')
-    })
-    db = getDb()
+      console.log("app listening on port" + 3000);
+    });
+    db = getDb();
   }
-})
+});
 
-app.get('/films', (req,res) =>{
+app.get("/films", (req, res) => {
+  let films = [];
 
-let films = []
-
-  db.collection('films')
-  .find()
-  .sort({ director: 1})
-  .forEach(film => films.push(film))
-  .then(() => {
-    res.status(200).json(films)
-  })
-  .catch(() => {
-    res.status(500).json({error: 'Could not fetch the data'})
-  })
-})
+  db.collection("films")
+    .find()
+    .sort({
+      director: 1,
+    })
+    .forEach((film) => films.push(film))
+    .then(() => {
+      res.status(200).json(films);
+    })
+    .catch(() => {
+      res.status(500).json({
+        error: "Could not fetch the data",
+      });
+    });
+});
